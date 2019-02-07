615d587a1b3acc1c0be266be03ef53881e93694d
Chris Del <drumgod101@gmail.com>
2019-02-07 15:39:59 -0500

    make gh-pages branch

---
+++ client/build/precache-manifest.47fa737b1e139c75ede3034c3d33cf41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0f9cf5f777930783336",
+    "url": "/plot-n-graph/static/js/runtime~main.d0f9cf5f.js"
+  },
+  {
+    "revision": "9698d18a8e21340d7a76",
+    "url": "/plot-n-graph/static/js/main.9698d18a.chunk.js"
+  },
+  {
+    "revision": "8582214a621808452b30",
+    "url": "/plot-n-graph/static/js/1.8582214a.chunk.js"
+  },
+  {
+    "revision": "9698d18a8e21340d7a76",
+    "url": "/plot-n-graph/static/css/main.e621115f.chunk.css"
+  },
+  {
+    "revision": "38cd9d28e1033c28c313d4466d406445",
+    "url": "/plot-n-graph/index.html"
+  }
+];
