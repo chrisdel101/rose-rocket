d36a4c74a28a8b33e43df6f901e0e716e1dfec15
Chris Del <drumgod101@gmail.com>
2019-02-07 16:05:35 -0500

    push build to GH-pages

---
+++ client/build/precache-manifest.37f93b258e47d81efa0f27714977e4d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0f9cf5f777930783336",
+    "url": "/plot-n-graph/static/js/runtime~main.d0f9cf5f.js"
+  },
+  {
+    "revision": "a570b65d4b11970b4218",
+    "url": "/plot-n-graph/static/js/main.a570b65d.chunk.js"
+  },
+  {
+    "revision": "f2b8f681060f86375fe9",
+    "url": "/plot-n-graph/static/js/1.f2b8f681.chunk.js"
+  },
+  {
+    "revision": "a570b65d4b11970b4218",
+    "url": "/plot-n-graph/static/css/main.be27eaef.chunk.css"
+  },
+  {
+    "revision": "c19bff6be625bea3371efde58f1cdcef",
+    "url": "/plot-n-graph/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/plot-n-graph/precache-manifest.f02b3fd924609e40b7535889c7692830.js"
+  "/plot-n-graph/precache-manifest.37f93b258e47d81efa0f27714977e4d9.js"
 );
 
 workbox.clientsClaim();
