727919ed0c82d2b4acdee34013e7de3fdcff3c8c
Chris Del <drumgod101@gmail.com>
2019-02-07 15:41:37 -0500

    more boxes

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -46,7 +46,7 @@ class Grid extends Component {
             previousLegY:0,
             partialLegStartCoords: "",
             partialLegEndCoords: "",
-            boxesToRender: Array.from({length: 1}, (v, i) => i),
+            boxesToRender: Array.from({length: 40000}, (v, i) => i),
             holdAllStopColorIndexes: [],
             holdAllLegColorArrs: [],
             holdingCompletedArrs: [],
