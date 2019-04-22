68582c898ea9f10298a08c6684b6d70c47a25fb9
Chris Del <drumgod101@gmail.com>
2019-04-22 16:17:05 -0400

    add build and server script

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var driver = require('./data/driver.json')
 const fs = require('fs')
 app.use(express.json())
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 console.log(driver)
